e56e3507aa5aff111aa65d48a14f38f66b52a50c
zhaodeezhu <zhaodeezhu@126.com>
2022-04-12 22:39:15 +0800

    feat: 🎸 增加普通队列

---
+++ src/Queue/Queue.ts
@@ -0,0 +1,58 @@
+
+interface IItem<T> {
+  [key:number]: T
+}
+
+export class Queue<T> {
+
+  /** 队尾的数量 */
+  count = 0;
+  /** 对头的指针 */
+  lowestCount = 0;
+  /** 数据项 */
+  items: IItem<T> = {};
+
+  /** 入列 */
+  enqueue(element: T) {
+    this.items[this.count++] = element;
+  }
+  /** 出列 */
+  dequeue() {
+    if(this.isEmpty()) {
+      return undefined;
+    }
+    const result = this.items[this.lowestCount];
+    delete this.items[this.lowestCount++];
+    return result;
+  }
+  /** 查看头元素 */
+  peek() {
+    return this.items[this.lowestCount];
+  }
+  /** 是否为空 */
+  isEmpty() {
+    return this.count - this.lowestCount === 0;
+  }
+  /** 大小 */
+  size() {
+    return this.count - this.lowestCount;
+  }
+  /** 清空 */
+  clear() {
+    this.items = {};
+    this.count = 0;
+    this.lowestCount = 0;
+  }
+  /** toString */
+  toString() {
+    if(this.isEmpty()) {
+      return '';
+    }
+    let str = `${this.items[this.lowestCount]}`;
+    for (let i = this.lowestCount + 1; i < this.count; i++) {
+      const item = this.items[i];
+      str += `,${item}`
+    }
+    return str;
+  }
+}
